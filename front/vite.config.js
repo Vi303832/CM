ee33e0aa23149737d6410e4f764ba237f72cb717
Akif <mehmetakiftanyerii@hotmail.com>
2025-05-05 15:58:05 +0300

    Deploy error fix

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [react(),
   tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: true,
+  },
+  server: {
+    port: 3000,
+    host: true
+  }
 })
